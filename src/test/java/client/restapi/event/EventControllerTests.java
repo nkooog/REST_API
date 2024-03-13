7c09af13655c6e83ec935ca3f24671e3380ec143
Jy_Park <nkooog@naver.com>
2024-03-13 21:42:42 +0900

    init commit

---
+++ src/test/java/client/restapi/event/EventControllerTests.java
@@ -0,0 +1,18 @@
+package client.restapi.event;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+
+}
