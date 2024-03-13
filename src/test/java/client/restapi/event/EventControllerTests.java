deb8b78c85bdd8193d53a668599805b793ed6e0f
Jy_Park <nkooog@naver.com>
2024-03-13 22:10:14 +0900

    tdd api init

--- src/test/java/client/restapi/event/EventControllerTests.java
+++ src/test/java/client/restapi/event/EventControllerTests.java
@@ -1,11 +1,24 @@
 package client.restapi.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("EventController tdd")
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
 public class EventControllerTests {
@@ -13,6 +26,33 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
 
+    @Test
+    public void createEvent() throws Exception{
+
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2024, 03, 13, 22, 00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2024, 03, 14, 22, 00))
+                .beginEventDateTime(LocalDateTime.of(2024, 03, 15, 22, 00))
+                .endEventDateTime(LocalDateTime.of(2024, 03, 16, 22, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("인천 미추홀구")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 본문에 json을 담아 보냄
+                        .accept(MediaTypes.HAL_JSON) // 응답받기 원한 타입
+                        .content(objectMapper.writeValueAsString(event)) // 요청 본문
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
+    }
 
 }
